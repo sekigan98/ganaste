9c79300ef32be001a96a927d43e42f372327055a
sekigan98 <sekigan98@gmail.com>
2025-07-01 18:11:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -52,9 +52,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const whatsappLogo = document.getElementById("whatsapp-logo");
 
   const links = [
-    "https://wa.link/wdd4bs", // 1 474 758 5814
-    "https://wa.link/lf8pvi", // 1 474 758 6757
-    "https://wa.link/dv5ph9", // 1 474 758 6403
+   // "https://wa.link/wdd4bs", // 1 474 758 5814
+   // "https://wa.link/lf8pvi", // 1 474 758 6757
+   // "https://wa.link/dv5ph9", // 1 474 758 6403
+
+    "https://wa.me/14747585814?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
+    "https://wa.me/14747586757?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
+    "https://wa.me/14747586403?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
   ];
 
   const randomIndex = Math.floor(Math.random() * links.length);
