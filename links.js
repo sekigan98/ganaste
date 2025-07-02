85ccb8c5cf47bc6aabfc549cb00721bb582ccee6
sekigan98 <sekigan98@gmail.com>
2025-07-02 15:33:19 -0300

    Create links.js

---
+++ links.js
@@ -0,0 +1,8 @@
+const links = [
+   // "https://wa.link/wdd4bs", // 1 474 758 5814
+   // "https://wa.link/lf8pvi", // 1 474 758 6757
+   // "https://wa.link/dv5ph9", // 1 474 758 6403
+
+    "https://wa.me/5491130738182?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
+    "https://wa.me/5491130736507?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
+  ];
