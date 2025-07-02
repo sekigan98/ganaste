a6b88c50a07af71a5b09366583a9e75b24be9822
sekigan98 <sekigan98@gmail.com>
2025-07-02 11:42:40 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -56,9 +56,8 @@ document.addEventListener("DOMContentLoaded", () => {
    // "https://wa.link/lf8pvi", // 1 474 758 6757
    // "https://wa.link/dv5ph9", // 1 474 758 6403
 
-    "https://wa.me/14747585814?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
-    "https://wa.me/14747586757?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
-    "https://wa.me/14747586403?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
+    "https://wa.me/5491130738182?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
+    "https://wa.me/5491130736507?text=Hola%2C%20vi%20la%20promo%20y%20quiero%20crear%20mi%20usuario",
   ];
 
   const randomIndex = Math.floor(Math.random() * links.length);
