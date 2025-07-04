54671d9e462cbf4521d8db12f5f89d87f47b1fd7
sekigan98 <sekigan98@gmail.com>
2025-07-03 21:40:20 -0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const numeros = [
+    "https://wa.me/5491130738182",
+    "https://wa.me/5491130736507"
+  ];
+  const elegido = numeros[Math.floor(Math.random() * numeros.length)];
+  const mensaje = "?text=Hola!%20Estoy%20interesado%20en%20más%20info";
 
+  setTimeout(() => {
+    window.location.href = elegido + mensaje;
+  }, 1500); // 1.5 segundos de espera
+});
